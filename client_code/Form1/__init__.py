4fb79213edbfb4314e147978b95a6c3e0636fec6
tabb99@gmail.com <tabb99@gmail.com>
2025-06-06 19:19:02 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +11,12 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    anvil.users.login_with_form()
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    user = anvil.users.get_user()
+    print(user['email'])
+
+
+    
